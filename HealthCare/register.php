dfab9eb101b25775135e7de04be9bbc5ed07d259
Rawnreff <rafenrafen1312@gmail.com>
2024-05-15 20:41:18 +0700

    initial commit

--- HealthCare/register.php
+++ HealthCare/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="style-reg.css">
+    <link rel="stylesheet" href="style_reg.css">
 </head>
 <body>
     <div class="regist-container">
