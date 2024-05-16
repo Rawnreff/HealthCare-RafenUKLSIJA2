8adf07436401e5c5da341b3434e393aa1a8e6864
Rawnreff <rafenrafen1312@gmail.com>
2024-05-16 09:01:22 +0700

    initial commit

--- HealthCare/healthcare (2).sql
+++ HealthCare/healthcare (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2024 at 03:56 AM
+-- Generation Time: May 16, 2024 at 04:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,24 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `article` (
   `id_article` int(11) NOT NULL,
-  `id_masterkey` int(11) NOT NULL,
-  `category` varchar(50) NOT NULL,
+  `id_personalization` int(11) NOT NULL,
   `title` varchar(225) NOT NULL,
   `content` varchar(225) NOT NULL,
   `additional_information` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `masterkey`
---
-
-CREATE TABLE `masterkey` (
-  `id_masterkey` int(11) NOT NULL,
-  `id_personalization` int(11) NOT NULL,
-  `key` varchar(225) NOT NULL,
-  `trend_key` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -63,6 +49,17 @@ CREATE TABLE `personalization` (
   `additional_preferences` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `personalization`
+--
+
+INSERT INTO `personalization` (`id_personalization`, `id_subscription`, `id_user`, `preferences`, `additional_preferences`) VALUES
+(2, 3, 9, 'Heavy Workout', 'Abs, Shoulder'),
+(4, 2, 12, 'Cardio', 'Running, Cycling'),
+(6, 1, 11, 'Clean Eating', 'Real Food, Vegetables'),
+(8, 7, 16, 'Yoga', 'Pilates'),
+(9, 8, 18, 'Swimming', 'None');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +73,19 @@ CREATE TABLE `subscription` (
   `plan_price` varchar(225) NOT NULL,
   `status` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subscription`
+--
+
+INSERT INTO `subscription` (`id_subscription`, `id_user`, `plan_name`, `plan_price`, `status`) VALUES
+(1, 11, 'premium plan', '20000', 'active'),
+(2, 12, 'premium plan', '20000', 'active'),
+(3, 9, 'premium plan', '20000', 'active'),
+(4, 13, 'free plan', '0', 'active'),
+(5, 14, 'free plan', '0', 'active'),
+(7, 16, 'premium plan', '20000', 'active'),
+(8, 18, 'premium plan', '20000', 'active');
 
 -- --------------------------------------------------------
 
@@ -99,7 +109,11 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id_user`, `name`, `username`, `email`, `password`, `level`) VALUES
 (9, 'admin', 'admin', 'admin@gmail.com', '1234', 'admin'),
 (11, 'user', 'user', 'user@gmail.com', '1234', 'user'),
-(12, 'userpremium', 'userprem', 'userprem@gmail.com', '4321', 'user');
+(12, 'userpremium', 'userpremium', 'userpremium@gmail.com', '4321', 'user'),
+(13, 'dummy1', 'dummy1', 'dummy1@gmail.com', 'dummy', 'user'),
+(14, 'dummy2', 'dummy2', 'dummy2@gmail.com', 'dummy', 'user'),
+(16, 'rafen', 'rafen', 'rafenn@gmail.com', 'rafen', 'user'),
+(18, 'fofon', 'fofon', 'fofon@gmail.com', 'fofon', 'user');
 
 --
 -- Indexes for dumped tables
@@ -110,14 +124,7 @@ INSERT INTO `user` (`id_user`, `name`, `username`, `email`, `password`, `level`)
 --
 ALTER TABLE `article`
   ADD PRIMARY KEY (`id_article`),
-  ADD KEY `fk_article_relation_masterkey` (`id_masterkey`);
-
---
--- Indexes for table `masterkey`
---
-ALTER TABLE `masterkey`
-  ADD PRIMARY KEY (`id_masterkey`),
-  ADD KEY `fk_masterkey_relation_personalization` (`id_personalization`);
+  ADD KEY `fk_personalization` (`id_personalization`);
 
 --
 -- Indexes for table `personalization`
@@ -151,28 +158,22 @@ ALTER TABLE `article`
   MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `masterkey`
---
-ALTER TABLE `masterkey`
-  MODIFY `id_masterkey` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `personalization`
 --
 ALTER TABLE `personalization`
-  MODIFY `id_personalization` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_personalization` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `subscription`
 --
 ALTER TABLE `subscription`
-  MODIFY `id_subscription` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_subscription` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
@@ -182,13 +183,7 @@ ALTER TABLE `user`
 -- Constraints for table `article`
 --
 ALTER TABLE `article`
-  ADD CONSTRAINT `fk_article_relation_masterkey` FOREIGN KEY (`id_masterkey`) REFERENCES `masterkey` (`id_masterkey`);
-
---
--- Constraints for table `masterkey`
---
-ALTER TABLE `masterkey`
-  ADD CONSTRAINT `fk_masterkey_relation_personalization` FOREIGN KEY (`id_personalization`) REFERENCES `personalization` (`id_personalization`);
+  ADD CONSTRAINT `fk_personalization` FOREIGN KEY (`id_personalization`) REFERENCES `personalization` (`id_personalization`);
 
 --
 -- Constraints for table `personalization`
--- HealthCare/user-premium/index.php
+++ HealthCare/user-premium/index.php
@@ -13,7 +13,7 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
-    <link rel="stylesheet" href="style_index.css"/>
+    <link rel="stylesheet" href="style_page.css"/>
     <script src="script.js"></script>
   </head>
   <body>
@@ -25,10 +25,9 @@
       </div>
       <div class="navigator">
         <a href="index.php" class="btn btn-active">Home</a>
-        <a href="loginpage.php" class="btn">Daily Workouts</a>
-        <a href="loginpage.php" class="btn">Healthy Foods</a>
-        <a href="loginpage.php" class="btn">Sleep Programs</a>
-        <a href="loginpage.php" class="btn">Login</a>
+        <a href="daily-workouts.php" class="btn">Daily Workouts</a>
+        <a href="healthy-foods.php" class="btn">Healthy Foods</a>
+        <a href="sleep-programs.php" class="btn">Sleep Programs</a>
         <div class="animation start-home1"></div>
       </div>
     </div>
@@ -40,10 +39,10 @@
         <p class="description">
           Embarking on a path to well-being involves embracing daily exercise, a balanced diet, and consistent sleep. Regular physical activity invigorates the body and sharpens the mind, fostering enhanced productivity. Paired with a wholesome diet, it fuels the body, improves mood, sustains energy, and boosts the immune system. A consistent sleep routine becomes a cornerstone, facilitating natural recovery and cognitive function. The persuasive appeal lies in the promise of a vibrant, energized life, empowering individuals to meet daily demands with resilience and savor the richness of each moment through elevated well-being.
         </p>
-        <a href="loginpage.php">Login First!</a>
+        <a href="daily-workouts.php">Find out with Daily Workouts</a>
       </div>
       <div class="banner-image">
-        <img src="user/Image/healthy-lifestyle-habit-illustration_726237-57.jpg" alt="" />
+        <img src="Image/healthy-lifestyle-habit-illustration_726237-57.jpg" alt="" />
       </div>
     </div>
 
@@ -51,14 +50,14 @@
     <div class="superior">
       <div class="superior-text">
         <div class="card">
-          <img class="card-img" src="user/Image/76bf36b24ae49ed25ee943280fc91699.jpg" alt="">
+          <img class="card-img" src="Image/76bf36b24ae49ed25ee943280fc91699.jpg" alt="">
           <div class="card-text">
             <h3>Recommendations and Mentoring</h3>
             <p>There are various recommendations and guidelines for you to lead a healthy lifestyle.</p>
           </div>
         </div>
         <div class="card">
-          <img class="card-img" src="user/Image/Ilustrasi-by-Halodoc-scaled.jpg" alt="">
+          <img class="card-img" src="Image/Ilustrasi-by-Halodoc-scaled.jpg" alt="">
           <div class="card-text">
             <h3>Tutorial Video</h3>
             <p>There are various tutorial videos to support your progress in adopting a healthy lifestyle.</p>
@@ -70,7 +69,7 @@
     <!-- motivation -->
     <div class="motivation">
       <div class="motivation-image">
-        <img src="user/Image/bodygoal.jpg" alt="" />
+        <img src="Image/bodygoal.jpg" alt="" />
       </div>
       <div class="motivation-text">
         <h2>
@@ -78,9 +77,9 @@
         </h2>
         <hr>
         <div class="image">
-          <img src="user/Image/workout.jpg" alt="" />
-          <img src="user/Image/healthy_breakfast.jpg" alt="" />
-          <img src="user/Image/sleep.jpg" alt="" />
+          <img src="Image/workout.jpg" alt="" />
+          <img src="Image/healthy_breakfast.jpg" alt="" />
+          <img src="Image/sleep.jpg" alt="" />
         </div>
       </div>
     </div>
@@ -92,7 +91,7 @@
       </div>
       <div class="footer-bot">
         <div class="profile-navigation">
-          <a href="loginpage.php" class="profile-link">Profile</a>
+          <a href="profile.php" class="profile-link">Profile</a>
           <span>|</span>
           <a href="https://www.instagram.com/raffortless/" class="profile-link">Instagram</a>
         </div>
