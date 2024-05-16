1952d6975c8036c111bc80aca8993363616697e6
Rawnreff <rafenrafen1312@gmail.com>
2024-05-16 14:29:50 +0700

    initial commit

--- HealthCare/login.php
+++ HealthCare/login.php
@@ -1,5 +1,4 @@
 <?php
-// mengaktifkan session pada php
 session_start();
 include 'connect.php';
 
@@ -7,36 +6,45 @@ $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$login = mysqli_query($mysqli,"select * from user where email='$email' or username='$username' and password='$password'");
+$login = mysqli_query($mysqli,"SELECT * FROM user WHERE (email='$email' OR username='$username') AND password='$password'");
 $cek = mysqli_num_rows($login);
 
 if($cek > 0){
-
     $data = mysqli_fetch_assoc($login);
 
-    // cek jika user login sebagai admin
-    if($data['level']=="admin"){
-        // buat session login dan username
+    if($data['level'] == "admin"){
         $_SESSION['email'] = $email;
         $_SESSION['username'] = $username;
         $_SESSION['level'] = "admin";
-        // alihkan ke halaman daashboard admin
-        header("location:admin/index.php");
+        header("Location: admin/index.php");
 
-    // cek jika user login sebagai user
-    }else if($data['level']=="user"){
-        // buat session login dan username
+    } else if($data['level'] == "user"){
         $_SESSION['email'] = $email;
         $_SESSION['username'] = $username;
         $_SESSION['level'] = "user";
-        // alihkan ke halaman daashboard user
-        header("location:user/index.php");
 
-    }else{
-        // alihkan ke halaman login kembali
-        header("location:loginpage.php");
+        $id_user = $data['id_user'];
+
+        $subscription_query = mysqli_query($mysqli, "SELECT plan_name FROM subscription WHERE id_user='$id_user'");
+        if (mysqli_num_rows($subscription_query) > 0) {
+            $subscription_data = mysqli_fetch_assoc($subscription_query);
+            $plan_name = $subscription_data['plan_name'];
+
+            if ($plan_name == 'premium plan') {
+                header("Location: user-premium/index.php");
+            } else if ($plan_name == 'free plan') {
+                header("Location: user/index.php");
+            } else {
+                header("Location: loginpage.php?pesan=plan_not_found");
+            }
+        } else {
+            header("Location: loginpage.php?pesan=no_subscription");
+        }
+
+    } else {
+        header("Location: loginpage.php?pesan=level_not_found");
     }
-}else{
-    header("location:loginpage.php?pesan=gagal");
+} else {
+    header("Location: loginpage.php?pesan=gagal");
 }
 ?>
