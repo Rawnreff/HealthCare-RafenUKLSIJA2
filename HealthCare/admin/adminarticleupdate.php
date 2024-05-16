f3cfa0edb426b0cfaa5251ab01a14ad0595721a9
Rawnreff <rafenrafen1312@gmail.com>
2024-05-16 18:08:10 +0700

    initial commit

---
+++ HealthCare/admin/adminarticleupdate.php
@@ -0,0 +1,84 @@
+<?php
+include '../connect.php';
+
+if(isset($_GET['id'])) {
+    $id_article = $_GET['id'];
+
+    if(isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $information = $_POST['information'];
+        $content = $_POST['content'];
+
+        $query = "UPDATE article SET title='$title', information='$information', content='$content' WHERE id_article='$id_article'";
+        $result = mysqli_query($mysqli, $query);
+
+        if($result) {
+            header("Location: adminsubs.php");
+            exit;
+        } else {
+            echo "Error: " . mysqli_error($mysqli);
+        }
+    }
+
+    $query = "SELECT * FROM subscription WHERE id_subscription='$id_subscription'";
+    $result = mysqli_query($mysqli, $query);
+    $data = mysqli_fetch_assoc($result);
+} else {
+    header("Location: adminsubs.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Article</title>
+    <link rel="icon" type="image/png" href="../logotitle.png">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Signika:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+    <link rel="stylesheet" href="style_update.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1 class="title">Update Article</h1>
+        </header>
+        <section class="form">
+        <form method="POST" action="">
+
+            <label for="plan_name">Plan Name:</label><br>
+                <select requiredid id="plan_name" name="plan_name" value="<?php echo $data['plan_name']; ?>">
+                    <option value="free plan">Free Plan</option>
+                    <option value="premium plan">Premium</option>
+                </select>
+
+            <label for="plan_price">Plan Price:</label><br>
+            <select requiredid id="plan_price" name="plan_price" value="<?php echo $data['plan_price']; ?>">
+                    <option value="0">Free</option>
+                    <option value="20000">Rp20.000,-</option>
+                    <option value="50000">Rp50.000,-</option>
+            </select>
+            
+            <label for="status">Status:</label><br>
+            <select requiredid id="status" name="status" value="<?php echo $data['status']; ?>">
+                    <option value="active">Active</option>
+                    <option value="inactive">Inactive</option>
+            </select>
+                
+                <br><br>
+                
+            <input type="submit" name="submit" value="Update" class="button">
+        </form>
+        </section>
+    </div>
+    <script src="main.js"></script>
+</body>
+</html>
