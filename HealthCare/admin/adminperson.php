435c9a268e01a07b6fdcc311f0ec18b929858ff4
Rawnreff <rafenrafen1312@gmail.com>
2024-05-16 10:16:32 +0700

    initial commit

--- HealthCare/admin/adminperson.php
+++ HealthCare/admin/adminperson.php
@@ -12,7 +12,7 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
-    <link rel="stylesheet" href="style_page.css">
+    <link rel="stylesheet" href="style_main.css">
     <script src="main.js"></script>
 </head>
 <body>
@@ -22,11 +22,11 @@
         </div>
         <i class='bx bx-menu' id="menu-icon"></i>
         <ul class="navbar">
-        <li><a href="index.php">Home</a></li>
-            <li><a href="adminuser.php">User</a></li>
-            <li><a href="adminsubs.php">Subscription</a></li>
+        <li><a href="index.php" class="btn-nav">Home</a></li>
+            <li><a href="adminuser.php" class="btn-nav">User</a></li>
+            <li><a href="adminsubs.php" class="btn-nav">Subscription</a></li>
             <li><a href="adminperson.php" class="btn-active">Personalization</a></li>
-            <li><a href="adminarticle.php">Article</a></li>
+            <li><a href="adminarticle.php" class="btn-nav">Article</a></li>
         </ul>
     </header>
     <section class="user">
