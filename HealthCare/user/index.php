923545aef3e02212afbaa86758e1139ca7370d41
Rawnreff <rafenrafen1312@gmail.com>
2024-05-16 08:56:04 +0700

    initial commit

--- HealthCare/user/index.php
+++ HealthCare/user/index.php
@@ -13,7 +13,7 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style_page.css"/>
     <script src="script.js"></script>
   </head>
   <body>
