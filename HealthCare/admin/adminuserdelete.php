dc036d2839681d868bd3951cbc50fea9a974eefc
Rawnreff <rafenrafen1312@gmail.com>
2024-05-26 13:28:39 +0700

    initial commit

--- HealthCare/admin/adminuserdelete.php
+++ HealthCare/admin/adminuserdelete.php
@@ -3,12 +3,24 @@ include '../connect.php';
 
 $id_user = $_GET['id'];
 
-$delete = mysqli_query($mysqli, "DELETE FROM user WHERE id_user = '$id_user'") or die(mysqli_error($mysqli));
+mysqli_begin_transaction($mysqli);
 
-if ($delete) {
+try {
+    $delete_subscription = mysqli_query($mysqli, "DELETE FROM subscription WHERE id_user = '$id_user'");
+    if (!$delete_subscription) {
+        throw new Exception("Gagal menghapus data dari tabel subscription: " . mysqli_error($mysqli));
+    }
+
+    $delete_user = mysqli_query($mysqli, "DELETE FROM user WHERE id_user = '$id_user'");
+    if (!$delete_user) {
+        throw new Exception("Gagal menghapus data dari tabel user: " . mysqli_error($mysqli));
+    }
+
+    mysqli_commit($mysqli);
     header("Location: adminuser.php");
     exit();
-} else {
-    echo "Gagal menghapus user.";
+} catch (Exception $e) {
+    mysqli_rollback($mysqli);
+    echo $e->getMessage();
 }
 ?>
